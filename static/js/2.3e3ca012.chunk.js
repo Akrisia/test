1bfa52d22a179a2d1c9723b708dc14ab558a6758
Iryna S <irynaradkovska@gmail.com>
2023-04-21 03:30:15 +0300

    Updates

---
+++ static/js/2.3e3ca012.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktest=self.webpackChunktest||[]).push([[2],{249:function(t,e,n){n.r(e),n.d(e,{default:function(){return i}});var s={title:"NotFound_title__ZCDYq"},u=n(184);function i(){return(0,u.jsx)("h2",{className:s.title,children:"Oops, the page does not exist here"})}}}]);
+//# sourceMappingURL=2.3e3ca012.chunk.js.map
