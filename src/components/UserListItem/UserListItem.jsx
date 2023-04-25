e4dcc983bec638b0cc2a30a49b7e02ee7d755ac7
Iryna S <irynaradkovska@gmail.com>
2023-04-26 00:06:37 +0300

    fix the picture

--- src/components/UserListItem/UserListItem.jsx
+++ src/components/UserListItem/UserListItem.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import logo from '../../images/logo.png';
-import picture from '../../images/picture.png';
 import FollowButton from '../FollowButton';
 import s from './UserListItem.module.css';
 
@@ -16,7 +15,6 @@ export default function UserListItem({ id, name, tweets, followers, avatar }) {
     return (
         <li className={s.item}>
             <img className={s.logo} src={logo} alt='logo' width="76" height="22" />
-            <img className={s.picture} src={picture} alt='abstract figures' width="308" height="168" />
             <div className={s.divider}>
                 <img className={s.avatar} src={avatar} alt={name} width="62" height="62" />
             </div>
