c214745ee38e5e7595381bdaf15b98423bc9c8eb
Iryna S <irynaradkovska@gmail.com>
2023-04-26 11:45:19 +0300

    move app to root

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
-import Navigation from '../components/Navigation';
-import Loader from '../components/Loader';
+import Navigation from './components/Navigation';
+import Loader from './components/Loader';
 import s from './App.module.css';
 
-const HomeView = lazy(() => import('../views/HomeView'));
-const TweetsView = lazy(() => import('../views/TweetsView'));
-const NotFound = lazy(() => import('../views/NotFound'));
+const HomeView = lazy(() => import('./views/HomeView'));
+const TweetsView = lazy(() => import('./views/TweetsView'));
+const NotFound = lazy(() => import('./views/NotFound'));
 
 export default function App() {
   return (
